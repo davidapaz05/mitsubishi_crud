d88fd012b4243d933499d392f30454375963b4cf
Davi da Paz Mota <davipaz125@gmail.com>
2024-12-05 20:11:37 -0300

    Add files via upload

---
+++ mitsubishi.sql
@@ -0,0 +1,48 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 04/12/2024 às 20:47
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `mitsubishi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `veiculos`
+--
+
+CREATE TABLE `veiculos` (
+  `modelo` varchar(100) NOT NULL,
+  `ano` int(11) NOT NULL,
+  `cor` varchar(50) DEFAULT NULL,
+  `placa` varchar(7) DEFAULT NULL,
+  `status` enum('disponivel','vendido') DEFAULT 'disponivel'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `veiculos`
+--
+
+INSERT INTO `veiculos` (`modelo`, `ano`, `cor`, `placa`, `status`) VALUES
+('Lancer', 2015, 'branco', 'pab0999', 'vendido');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
